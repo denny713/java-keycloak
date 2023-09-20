c9f0fd0fd8a9a4bd6bd16b4d82f52207fa80d0fe
Denny Afrizal <denny.afrizal713@gmail.com>
2023-09-20 08:20:36 +0700

    Project Skeleton

---
+++ src/main/java/com/denny/keycloak/DennyKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.denny.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DennyKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DennyKeycloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/denny/keycloak/DennyKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denny.keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DennyKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
